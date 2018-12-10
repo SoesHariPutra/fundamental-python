78975adc4b420b19242430603aa4626f2494e7be
Soes Hari Putra <38636474+soeshariputra@users.noreply.github.com>
2018-12-10 11:56:56 +0700

    Fundamental Python sampai Control Flow

---
+++ main.py
@@ -0,0 +1,69 @@
+"""
+Komentar panjang
+"""
+print("Hello World!")
+# variables type
+gedung = "BPK"
+tinggi = 8
+PI = "Private Investigator"
+pi = 3.14
+is_alive = True
+is_alive = "XXX"
+#tinggi = True # tipe variable bs berubah2
+print(gedung)
+print(tinggi)
+print(pi)
+print(PI, is_alive)
+
+# list
+hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu']
+print(hari)
+print(hari[0])
+print(hari[len(hari)-1])
+
+#dictionary
+antonim = {}
+antonim['tinggi'] = 'pendek'
+antonim['gelap'] = 'terang'
+antonim['jauh'] = 'dekat'
+print('Antonim dari jauh adalah', antonim['jauh'])
+
+#SEQUENTIAL FLOW
+
+
+#CONTROL FLOW/IF
+if is_alive == True:
+    print('Hidup')
+
+if is_alive and tinggi > 7:
+    print('Hidup, ketinggian, jadi mati')
+
+if is_alive == True:
+    print('Hidup')
+elif is_alive == False:
+    print('Mati')
+else:
+    print('Ga Jelas')
+
+#LOOP
+#MENGULANG KODE BEBERAPA KALI SAMPAI KONDISI TERTENTU
+#FOR untuk perulangan yang pasti
+for i in range (0, 3):
+    print(i)
+
+#WHILE
+#Digunakan untuk perulangan yang jumlahnya tidak pasti
+#namun kondisi berhentinya pasti
+i = 0
+while is_alive:
+    print('Hidup')
+    i = i + 1
+    if i == 10:
+        is_alive = False
+
+#FOR UNTUK LIST
+for h in hari:
+    print(hari)
+
+for h in range(0, len(hari)-1):
+    print(hari(h))
